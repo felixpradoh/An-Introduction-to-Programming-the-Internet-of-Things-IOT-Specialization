586d9d0c3811d7adc73ecf05bf60fb2364189d30
felixpradoh <felixpradoh@gmail.com>
2023-02-22 22:15:13 +0100

    Week_3 100% update

---
+++ Interfacing with the Arduino/Week_3/EEPROM Sketch.ino
@@ -0,0 +1,35 @@
+#include <EEPROM.h> //Library needed for the write-read EEPROM
+
+int address;
+int dataToWrite;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+
+void loop() 
+{
+  String buffer = "";
+  buffer = Serial.readString();
+
+  if(buffer.startsWith("read")) {
+    address = buffer.substring(buffer.indexOf(' ') +1).toInt(); 
+    Serial.print("Data from the Address : ");
+    Serial.print(address);
+    Serial.println();
+    Serial.println(EEPROM.read(address)); 
+
+  } else if(buffer.startsWith("write")) {
+    address = buffer.substring(6,7).toInt();
+    dataToWrite = buffer.substring(8).toInt();
+    Serial.print("Written ");
+    Serial.print(dataToWrite);
+    Serial.println();
+    Serial.print("to the Address: ");
+    Serial.print(address);
+    Serial.println();
+    EEPROM.write(address,dataToWrite); 
+  }
+}
