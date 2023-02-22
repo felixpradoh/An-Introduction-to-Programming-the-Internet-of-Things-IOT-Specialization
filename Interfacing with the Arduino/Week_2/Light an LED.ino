faacddaf80c3b589c45635a11859b0a7f92d503f
felixpradoh <felixpradoh@gmail.com>
2023-02-22 20:02:35 +0100

    Week 2 100% updated

--- Interfacing with the Arduino/Week_2/Light an LED.ino
+++ Interfacing with the Arduino/Week_2/Light an LED.ino
@@ -1,6 +1,6 @@
-#define LED_PIN    13
+#define LED_PIN    9
 #define SENSOR_PIN A0
-#define BRIGHTNESS_THRESHOLD 900
+#define BRIGHTNESS_THRESHOLD 300
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -9,5 +9,6 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(SENSOR_PIN);
   int state = (sensorValue <= BRIGHTNESS_THRESHOLD) ? HIGH : LOW;
+  // delay(1000); adds a delay of 1 second for the turn on
   digitalWrite(LED_PIN, state);
 }
