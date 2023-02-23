2726b43d4fe95eb1fc50a2734524d26fa60394e2
felixpradoh <felixpradoh@gmail.com>
2023-02-23 08:45:07 +0100

    Week 3 100% complete

---
+++ The Raspberry Pi Platform and Python Programming for the Raspberry Pi/Week 3/Module 3 Peer Assignment.py
@@ -0,0 +1,14 @@
+# Let's generate an empty list
+my_list = []
+
+# Introduce numbers and append them to the list
+x = int(input('Introduce first number:  '))
+my_list.append(x)
+y = int(input('Introduce second number: '))
+my_list.append(y)
+z = int(input('Introduce third number:  '25))
+my_list.append(z)
+
+#sort the list in ascending order and print it
+my_list.sort()
+print('The list ordered is: {}'.format(my_list))
